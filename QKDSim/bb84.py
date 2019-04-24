12d64d9aa7a847bf292b0cfe9df1cf3a2d365272
James Slack-Smith <jpslacksmith@hotmail.com.au>
2019-04-24 18:06:41 +1000

    add runbb84 module

--- QKDSim/bb84.py
+++ QKDSim/bb84.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 24 17:27:45 2019
+Created on Wed Apr 24 18:06:01 2019
 
 @author: James
 """
 
-
-def main():
-    """
-    Simulate QKD with BB84 protocol
-    """
-    pass
-
-
 if __name__ == '__main__':
-    main()
+    pass
