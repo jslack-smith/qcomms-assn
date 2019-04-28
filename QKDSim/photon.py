e0c16d24253a38b90aa3bdd11a55ed800b8b435f
James Slack-Smith <jpslacksmith@hotmail.com.au>
2019-04-28 20:13:33 +1000

    remove runbb84

--- QKDSim/photon.py
+++ QKDSim/photon.py
@@ -130,3 +130,4 @@ if __name__ == '__main__':
     print('{}\nM({})\n{} trials'.format(Photon(theta).arrow(), alpha, N))
     print('Average outcome: {}'.format(avg_outcome))
     print(photon)
+    
