5a2c958c82774b3f6e4ad862faa5b962df90ba3f
Emmoter <49976967+Emmoter@users.noreply.github.com>
2019-05-01 13:57:25 +1000

    Privacy ampld added

--- qkdsim/errorcorrection.py
+++ qkdsim/errorcorrection.py
@@ -2,51 +2,60 @@
 """
 This module contains functions related to error correction
 """
-
-
-def parity_creation(self):
+import numpy as np
+def parity_check(key):
     """
     * Create parity of keys.
     This method will add one last bit to the key with the parity check.
     Parity bit = 1 if parity even, 0 if uneven
     """
-    a = self.sender.key
-    b = self.reciever.key
-    a_parity = 0
-    b_parity = 0
-    if ((a.sum(axis=0) % 2) == 0):
-        a_parity = 1
-        a.append(a_parity)
+    parity = 0 #Uneven parity
+    if ((sum(key) % 2) == 0):
+        parity = 1 #Even parity
 
-    if ((b.sum(axis=0) % 2) == 0):
-        b_parity = 1
-        b.append(b_parity)
-    return
+    return parity
 
 
-def parity_check(self):
-    """
-    * Check parity of keys to see if there are errors on Alice and Bob keys
-    """
-    a = self.sender.key
-    b = self.reciever.key
-    a_parity = a[-1]  # get parity bit
-    b_parity = b[-1]  # get parity bit
-    error_corr = False
-
-    self.reciever.cl_chan.send(b_parity)
-    self.sender.cl_chan.send(a_parity)
-
-    if((self.sender.cl_chan.receive() == b_parity)
-            and (self.receiver.cl_chan.receive() == a_parity)):
-        print("Sucesful parity check")
-    else:
-        print("Unsucesful parity check")
-        # use to decide if we need to perform error corr or not
-        error_corr = True
-
-    return error_corr
-
-
+def errorCorrection(a_key,b_key,cl_chan):
+        """
+        Keys must be np arrays of 7 bit long and both keys must have at MOST 1 error!
+        """
+        a = a_key
+        b = b_key
+        
+        hMatrix = np.matrix([[1, 1, 1, 0, 1, 0, 0], [1, 1, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 0, 1]])
+        sA = hMatrix * a.reshape((7, 1))
+        for i in range(0,len(sA)):
+            if(sA[i] % 2 == 0):
+                sA[i] =0
+            else:
+                sA[i] =1
+        cl_chan.send(sA)
+        
+        sB = hMatrix * b.reshape((7, 1))
+        for i in range(0,len(sB)):
+            if(sB[i] % 2 == 0):
+                sB[i] =0
+            else:
+                sB[i] =1
+        sA_received = cl_chan.receive()
+        s = sB - sA_received
+        for i in range(0,len(sB)):
+            if(s[i]  == -1):
+                s[i] =1
+        e = np.zeros(7)
+        compareArr = s.reshape((3, 1)) == hMatrix
+        checkArray = np.array([[True], 
+                               [True], 
+                               [True]])
+        i = 0
+        for i in range (0,7):
+            checkColumn = compareArr[:,[i]]
+            if(((checkArray.reshape((3, 1))) == (checkColumn.reshape((3, 1)))).all()):
+                e[i] = 1
+            else:
+                pass
+        
+        return (b - e)
 if __name__ == '__main__':
     pass
